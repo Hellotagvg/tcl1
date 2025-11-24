5c2164bff2e466ac233561555d486093da050c9e
Hellotagvg <138308471+Hellotagvg@users.noreply.github.com>
2025-11-24 09:08:51 +0000

    Add identifier check in main function

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import calculator as c
 import pybybit as pb
 
 def main(identifier,price_a,price_b,symbol,type):
+    
+  if identifier == 1:  
     max_wait = int()
     key_dict = {
     "acc1": {
@@ -10,9 +12,6 @@ def main(identifier,price_a,price_b,symbol,type):
         "api_secret": "5MOxtE17mWBK3KqZZSoesXIoq5os7C1WEHHP"
     }
 }
-
-
-
     dict_a,dict_b = c.tcl_calc(price_a,price_b,symbol,type)
     pb.trade_tcl(key_dict,dict_a,dict_b, max_wait_seconds = 6000)
 
@@ -21,4 +20,3 @@ td_email = 'hellotagvg1@gmail.com'
 em.start_spam_email_listener_recent(td_email,callback = main,poll_interval=3)
 
     
-
